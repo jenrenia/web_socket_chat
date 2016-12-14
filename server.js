2f9529a5a0df6b52295d8d5638ebb4121b1b56dd
jenrenia <z1x212qw@mail.ru>
2016-12-14 22:19:47 +0200

    Created Web Sockets server .Little demo.

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-var http = require('http');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(8080);
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
